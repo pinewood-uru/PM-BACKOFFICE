36522a00f839ee8d49d769613430663e0723c8c1
fran <f.avellanal96@gmail.com>
2024-12-11 20:37:05 -0300

    f

--- CONTROLLERS/getproyectobyid.js
+++ CONTROLLERS/getproyectobyid.js
@@ -2,7 +2,7 @@ import { ModeloProyecto } from "../DATABASE/models/ModeloProyecto.js";
 
 export const getProyectobyID = (req, res, next) => {
     const idProyecto = req.params.id;
-    ModeloProyecto.findOne({ id: idProyecto })
+    ModeloProyecto.findOne({ token: idProyecto })
     .then((data) => {
         if (!data) {
             throw new Error(`No se ha encontrado el Proyecto con id ${idProyecto}`);
@@ -16,11 +16,11 @@ export const getProyectobyID = (req, res, next) => {
 }
 
 export const getProyectobytoken = (req, res, next) => {
-    const tokenproyecto = req.params.id;
-    ModeloProyecto.findOne({ token: tokenproyecto })
+    const idProyecto = req.params.id;
+    ModeloProyecto.findOne({ id: idProyecto })
     .then((data) => {
         if (!data) {
-            throw new Error(`No se ha encontrado el Proyecto con Token ${tokenproyecto}`);
+            throw new Error(`No se ha encontrado el Proyecto con Token ${idProyecto}`);
         } else {
             res.json(data);
         }
